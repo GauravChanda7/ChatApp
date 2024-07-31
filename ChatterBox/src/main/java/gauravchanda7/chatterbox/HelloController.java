03d046af460be70bee31a0b3216fa07aa30792ff
Gaurav <gauravchanda2022@gmail.com>
2024-08-01 02:09:31 +0530

    create project and setup for JavaFX using Maven
    created the project, added JavaFX libraries, connected SceneBuilder, connected Github with Intellij IDE

---
+++ ChatterBox/src/main/java/gauravchanda7/chatterbox/HelloController.java
@@ -0,0 +1,14 @@
+package gauravchanda7.chatterbox;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ ChatterBox/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module gauravchanda7.chatterbox {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens gauravchanda7.chatterbox to javafx.fxml;
+    exports gauravchanda7.chatterbox;
+}
